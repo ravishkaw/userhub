975476b0a16a414b4a65c3e212f33329d5fe98ef
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2025-08-27 16:06:32 +0530

    Add user routes for CRUD operations

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
+router.get("/", getAllUsers);
+router.post("/", createUser);
+router.get("/:id", getUserById);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+module.exports = router;
