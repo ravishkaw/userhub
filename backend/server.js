a6ae3cc944dc30795cd5a52e34ab6ec1c1b1d7fb
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2025-08-27 16:06:15 +0530

    Initialize server with Express, CORS, and route setup

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const { poolPromise } = require("./config/db");
+const protect = require("./middleware/authMiddleware");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", protect, userRoutes);
+
+const startServer = async () => {
+  try {
+    await poolPromise;
+    console.log("Database connected successfully");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.error(
+      "Failed to start server due to database connection error:",
+      err
+    );
+    process.exit(1);
+  }
+};
+
+startServer();
